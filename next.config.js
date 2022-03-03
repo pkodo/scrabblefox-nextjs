14074981901d41917604f9075e4c79bc3ad2ac03
Paul Kodolitsch <paul@Pauls-MacBook-Pro.local>
2022-03-03 20:25:13 +0100

    new ga tag

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_GA_ID: 'UA-221063212-1',
+    NEXT_PUBLIC_GA_ID: 'G-KFLTYRB8V8',
     ADSENSE_ID: 'ca-pub-4456810469040806'
   }
 }
